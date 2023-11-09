e22bbbc7f907f15263f7c45a54c28c16f1de4d44
JORAM KIMATA <joram.kimata@ega.go.tz>
2023-11-09 14:40:19 +0300

    add fix

--- INKIPER/Dtos/DepositDto.cs
+++ INKIPER/Dtos/DepositDto.cs
@@ -7,6 +7,8 @@ public class DepositDto
     public record AccountRecord(string Uuid, string Name);
     public AccountRecord Account { get; set; }
     public string AmountDeposited { get; set; }
+    
+    public string CurrentAccountBalance { get; set; }
     public DateTime? DepositedDate { get; set; }
     
     public string Description { get; set; }
