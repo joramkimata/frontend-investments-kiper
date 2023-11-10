751e4a6660d893605ada1f18f2e2e5ace274e4df
JORAM KIMATA <joram.kimata@ega.go.tz>
2023-11-10 13:53:34 +0300

    add fix

--- INKIPER/GraphQL/Inputs/Deposits/DepositsInput.cs
+++ INKIPER/GraphQL/Inputs/Deposits/DepositsInput.cs
@@ -5,5 +5,7 @@ public class DepositsInput
     public required string accountsUid { get; set; }
     public required float amountDeposited { get; set; }
     public required string description { get; set; }
-    public required string depositedDate { get; set; }
+    public required string? depositedDate { get; set; }
+    
+    public required float currentBalance { get; set; }
 }
